3cbe783d25522c0f1a893c275901f12e8b7ef52f
Kevin John Kiely <kevinjohnkiely1@hotmail.com>
2023-03-04 16:45:54 +0000

    Refactoring routes and basic middlewares

--- app.js
+++ app.js
@@ -1,18 +1,76 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({
-    message: 'Hello from bgafc',
-    app: 'bfastc',
-  });
+// 1) MIDDELWARES
+app.use(morgan('dev'))
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('herro from miffleware!!');
+  next();
 });
 
-app.post('/', (req, res) => {
-    res.send('You can post to this endpoint!')
-})
+const players = [
+  {
+    _id: 'asljf39083108908',
+    firstName: 'Michael',
+    lastName: 'Aherne',
+  },
+];
 
+// 2) ROUTE HANDLERS
+const getAllPlayers = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    playerTotal: players.length,
+    data: {
+      players: players,
+    },
+  });
+};
+
+const getPlayer = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    playerTotal: players.length,
+    data: {
+      players: players,
+    },
+  });
+};
+
+const createPlayer = (req, res) => {
+  console.log(req.body);
+  res.send('Done');
+};
+
+const updatePlayer = (req, res) => {
+  res.status(200).json({
+    status: 'Success',
+    data: {
+      player: 'Updated PLAYER goes here...',
+    },
+  });
+};
+
+const deletePlayer = (req, res) => {
+  res.status(204).json({
+    status: 'Success',
+    data: null,
+  });
+};
+
+// 3) ROUTES
+app.route('/api/v1/players').get(getAllPlayers).post(createPlayer);
+app
+  .route('/api/v1/players/:slug')
+  .get(getPlayer)
+  .patch(updatePlayer)
+  .delete(deletePlayer);
+
+// 4) START SERVER
 const port = 1984;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
