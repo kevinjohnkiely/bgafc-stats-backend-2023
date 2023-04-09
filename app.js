620ae28e846c86e55c362a574b3eca45af6b6f45
Kevin John Kiely <kevinjohnkiely1@hotmail.com>
2023-04-09 14:54:36 +0100

    Added user routes

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
-
 app.use(express.json());
+app.use(morgan('dev'));
 
 const getAllPlayers = (req, res) => {
   res.status(200).json({
@@ -42,12 +43,53 @@ const deletePlayer = (req, res) => {
   });
 };
 
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined'
+  })
+}
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined'
+  })
+}
+
+const getOneUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined'
+  })
+}
+
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined'
+  })
+}
+
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined'
+  })
+}
+
 app.route('/api/v1/players').get(getAllPlayers).post(createPlayer);
 app
   .route('/api/v1/players/:playerSlug')
   .get(getOnePlayer)
   .patch(updatePlayer)
   .delete(deletePlayer);
+
+app.route('/api/v1/users').get(getAllUsers).post(createUser);
+app
+  .route('/api/v1/users/:id')
+  .get(getOneUser)
+  .patch(updateUser)
+  .delete(deleteUser);
 
 const port = 1984;
 app.listen(port, () => {
