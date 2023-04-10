741ea245f4254e3368b13d16d02cdd9504ce34dc
Kevin John Kiely <kevinjohnkiely1@hotmail.com>
2023-04-10 16:57:35 +0100

    Setup connection to mongodb

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 dotenv.config({ path: './config.env' });
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<THE_PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('DB Successfully Connected!');
+  });
 
 const port = process.env.PORT || 1984;
 app.listen(port, () => {
