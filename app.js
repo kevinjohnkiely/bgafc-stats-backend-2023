5547684269702a52439a11f1520ed1ab99f90a9c
Kevin John Kiely <kevinjohnkiely1@hotmail.com>
2023-04-12 09:36:53 +0100

    Improved error handler

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/errorHandling/appError');
+const globalErrorHandler = require('./utils/errorHandling/globalErrorHandler');
+
 const playerRouter = require('./routes/playerRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -18,24 +21,9 @@ app.use('/api/v1/players', playerRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'fail',
-  //   message: `Cannot find ${req.originalUrl} on this server!`,
-  // });
-  // next();
-  const err = new Error(`Cannot find ${req.originalUrl} on this server!`);
-  err.status = 'fail';
-  err.statusCode = 404;
-  next(err);
+  next(new AppError(`Cannot find ${req.originalUrl} on this server!`, 404));
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
